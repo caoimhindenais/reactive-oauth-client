f796dc1f38513147f36ebc09285e883b9bdf36e8
caoimhindenais <caoimhin.denais@gmail.com>
2019-09-24 09:36:44 +0200

    Removing default app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {TossComponent} from "./components/toss/toss.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'toss', pathMatch: 'full'},
+  {path :'toss', component: TossComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
